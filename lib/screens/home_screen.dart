a11af2ad9cdf7c659cabbc3ff9a5ecde789845b2
npnhannguyen <nhan.nguyen@netpower.no>
2019-09-06 18:41:21 +0700

    Add home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeState extends State<HomeScreen> {
+  int tabIndex = 0;
+  void _changeTabIndex(index) {
+    setState(() {
+      tabIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var data = EasyLocalizationProvider.of(context).data;
@@ -15,7 +22,52 @@ class _HomeState extends State<HomeScreen> {
       child: Scaffold(
         body: Center(
           child: Text(AppLocalizations.of(context).tr('home'))
-        )
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+        // Disable title of bar item
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Colors.white,
+          currentIndex: tabIndex,
+          selectedItemColor: Color.fromRGBO(75, 134, 180, 1),
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed ,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              title: new Text('')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.date_range,
+              ),
+              title: new Text('')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.list,
+              ),
+              title: new Text('')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+              ),
+              title: new Text('')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+              ),
+              title: new Text('')
+            )
+          ],
+          onTap: (index){
+            _changeTabIndex(index);
+          },
+        ),
       )
     );
   }
