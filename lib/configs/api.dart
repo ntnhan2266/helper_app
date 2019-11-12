31e70436f6356630ba78ce95da26b7ca4fee51a6
Nhan Nguyen <ntnhan2266@gmail.com>
2019-11-13 00:32:22 +0700

    Fix cast type

--- lib/configs/api.dart
+++ lib/configs/api.dart
@@ -1,5 +1,5 @@
 class APIConfig {
-  // static const baseURL = 'http://ec2-13-229-209-71.ap-southeast-1.compute.amazonaws.com/api/';
+  // static const baseURL = 'http://ec2-13-229-209-71.ap-southeast-1.compute.amazonaws.com/api/';/
   static const baseURL = 'http://10.0.2.2:4000/api';
   static const hostURL = 'http://10.0.2.2:4000';
   // static const hostURL = 'http://ec2-13-229-209-71.ap-southeast-1.compute.amazonaws.com';
