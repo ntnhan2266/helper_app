10d0e37c11e9e38a9fb47fd3b111e24f2809c35a
Hien Le Duy <hienld.citynow@gmail.com>
2019-12-04 21:51:54 +0700

    add about screen

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -36,16 +36,16 @@ class _AboutScreenState extends State<AboutScreen> {
   String _getTitle(int page) {
     switch (page) {
       case 0:
-        return "About App";
+        return AppLocalizations.of(context).tr('about_app');
         break;
       case 1:
-        return "Teacher";
+        return AppLocalizations.of(context).tr('teacher');
         break;
       case 2:
-        return "Student";
+        return AppLocalizations.of(context).tr('student');
         break;
       default:
-        return "About App";
+        return AppLocalizations.of(context).tr('about_app');
     }
   }
 
@@ -63,7 +63,7 @@ class _AboutScreenState extends State<AboutScreen> {
     );
   }
 
-  Widget _renderContents(int page) {
+  Widget _renderPages(int page) {
     return PageView(
       controller: PageController(
         initialPage: _page,
@@ -93,33 +93,153 @@ class _AboutScreenState extends State<AboutScreen> {
               letterSpacing: 3,
             ),
           ),
-          Container(
-            alignment: Alignment.center +
-                Alignment((toPage - fromPage) * _kViewportFraction, 0.0),
-            width: (MediaQuery.of(context).size.width - 100) * resizeFactor,
-            height: (MediaQuery.of(context).size.height - 200) * resizeFactor,
-            child: Center(
-              child: Padding(
-                padding: EdgeInsets.only(top: 20.0),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  elevation: 25.0,
-                  child: Container(
-                      // height: MediaQuery.of(context).size.height - 250,
-                      // width: MediaQuery.of(context).size.width - 150,
-                      // child: Container(
-                      //   margin: EdgeInsets.only(top: 30.0, bottom: 30.0),
-                      //   // child: child,
-                      // ),
-                      ),
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                elevation: 25.0,
+                child: Container(
+                  alignment: Alignment.center +
+                      Alignment((toPage - fromPage) * _kViewportFraction, 0.0),
+                  width:
+                      (MediaQuery.of(context).size.width - 100) * resizeFactor,
+                  height:
+                      (MediaQuery.of(context).size.height - 200) * resizeFactor,
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: _renderContents(fromPage, resizeFactor),
                 ),
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _renderContents(int page, double resizeFactor) {
+    switch (page) {
+      case 0:
+        return _renderAppContents(resizeFactor);
+        break;
+      case 1:
+        return _renderTeacherContents(resizeFactor);
+        break;
+      case 2:
+        return _renderStudentContents(resizeFactor);
+        break;
+      default:
+        return _renderAppContents(resizeFactor);
+    }
+  }
+
+  Widget _renderContentPrimaryText(String text, double resizeFactor) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          fontSize: 20 * resizeFactor,
+          fontWeight: FontWeight.w500,
+          color: Theme.of(context).primaryColor),
+    );
+  }
+
+  Widget _renderContentNormalText(String text, double resizeFactor) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 14 * resizeFactor,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _renderAppContents(double resizeFactor) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Image.asset(
+          'assets/images/logo_x5.png',
+          fit: BoxFit.fill,
+          height: MediaQuery.of(context).size.width * 0.3 * resizeFactor,
+          width: MediaQuery.of(context).size.width * 0.3 * resizeFactor,
+        ),
+        Column(
+          children: <Widget>[
+            _renderContentPrimaryText("Smart Rabbit", resizeFactor),
+            SizedBox(height: 10.0 * resizeFactor),
+            _renderContentNormalText(
+                AppLocalizations.of(context).tr('about_app_content'),
+                resizeFactor),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _renderTeacherContents(double resizeFactor) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Image.asset(
+          'assets/images/avt_default.png',
+          fit: BoxFit.fill,
+          height: MediaQuery.of(context).size.width * 0.3 * resizeFactor,
+          width: MediaQuery.of(context).size.width * 0.3 * resizeFactor,
+        ),
+        Column(
+          children: <Widget>[
+            _renderContentPrimaryText("Ths. Nguyễn Đình Thành", resizeFactor),
+            SizedBox(height: 10.0 * resizeFactor),
+            _renderContentNormalText(
+                AppLocalizations.of(context).tr('about_app_teacher_content'),
+                resizeFactor),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _renderStudentContents(double resizeFactor) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.width * 0.25 * resizeFactor,
+          width: MediaQuery.of(context).size.width * 0.25 * resizeFactor,
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/avt_default.png'),
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            _renderContentPrimaryText("Nguyễn Thanh Nhân", resizeFactor),
+            SizedBox(height: 5.0 * resizeFactor),
+            _renderContentNormalText(
+                AppLocalizations.of(context).tr('about_app_student_content'),
+                resizeFactor),
+          ],
+        ),
+        Container(
+          height: MediaQuery.of(context).size.width * 0.25 * resizeFactor,
+          width: MediaQuery.of(context).size.width * 0.25 * resizeFactor,
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/avt_default.png'),
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            _renderContentPrimaryText("Lê Duy Hiển", resizeFactor),
+            SizedBox(height: 5.0 * resizeFactor),
+            _renderContentNormalText(
+                AppLocalizations.of(context).tr('about_app_student_content'),
+                resizeFactor),
+          ],
+        ),
+      ],
     );
   }
 
@@ -133,7 +253,7 @@ class _AboutScreenState extends State<AboutScreen> {
           child: Stack(
             children: <Widget>[
               _renderBackground(_page),
-              _renderContents(_page),
+              _renderPages(_page),
               Positioned(
                 top: 0.0,
                 left: 0.0,
