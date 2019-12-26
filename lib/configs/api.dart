fe2f75b286acbdf232be43aaf02db55cec9dd9c4
Nhan Nguyen <ntnhan2266@gmail.com>
2019-12-26 14:27:20 +0700

    Update icon

--- lib/configs/api.dart
+++ lib/configs/api.dart
@@ -1,12 +1,12 @@
 class APIConfig {
   // -- Production
-  // static const baseURL = 'http://ec2-13-229-209-71.ap-southeast-1.compute.amazonaws.com/api/';
-  // static const hostURL = 'http://ec2-13-229-209-71.ap-southeast-1.compute.amazonaws.com';
+  static const baseURL = 'http://ec2-13-229-209-71.ap-southeast-1.compute.amazonaws.com/api/';
+  static const hostURL = 'http://ec2-13-229-209-71.ap-southeast-1.compute.amazonaws.com';
   // -- IOS Dev
   // static const baseURL = 'http://localhost:4000/api';
   // static const hostURL = 'http://localhost:4000';
   // -- Android Dev
-  static const baseURL = 'http://10.0.2.2:4000/api';
-  static const hostURL = 'http://10.0.2.2:4000';
+  // static const baseURL = 'http://10.0.2.2:4000/api';
+  // static const hostURL = 'http://10.0.2.2:4000';
   static const searchByAddressURL = 'https://maps.googleapis.com/maps/api/geocode/json';
 }
