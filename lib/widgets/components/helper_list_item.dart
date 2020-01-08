ebcba643b235f479eb4ed028fa5c303bb63ceade
Hien Le Duy <hienld.citynow@gmail.com>
2020-01-08 16:34:08 +0700

    fix search helper

--- lib/widgets/components/helper_list_item.dart
+++ lib/widgets/components/helper_list_item.dart
@@ -2,7 +2,9 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:intl/intl.dart';
-import 'package:smart_rabbit/configs/api.dart';
+
+import '../../configs/api.dart';
+import '../../utils/route_names.dart';
 
 class HelperListItem extends StatefulWidget {
   final maid;
@@ -28,69 +30,74 @@ class _HelperListItemState extends State<HelperListItem> {
   @override
   Widget build(BuildContext context) {
     final numericFormatter = new NumberFormat("#,###", "en_US");
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 10.0),
-      margin: EdgeInsets.symmetric(vertical: 3.0, horizontal: 5.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width / 4,
-            height: MediaQuery.of(context).size.width / 4,
-            padding: EdgeInsets.all(10.0),
-            margin: EdgeInsets.only(right: 10.0),
-            child: CircleAvatar(
-              backgroundImage: widget.maid['avatar'] != null
-                  ? NetworkImage(APIConfig.hostURL + widget.maid['avatar'])
-                  : AssetImage('assets/images/avt_default.png'),
-              backgroundColor: Colors.transparent,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(helperDetailRoute, arguments: widget.maid['_id']);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        margin: EdgeInsets.symmetric(vertical: 3.0, horizontal: 5.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width / 4,
+              height: MediaQuery.of(context).size.width / 4,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.only(right: 10.0),
+              child: CircleAvatar(
+                backgroundImage: widget.maid['avatar'] != null
+                    ? NetworkImage(APIConfig.hostURL + widget.maid['avatar'])
+                    : AssetImage('assets/images/avt_default.png'),
+                backgroundColor: Colors.transparent,
+              ),
             ),
-          ),
-          Flexible(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  widget.maid['name'] ?? "-",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  (widget.maid['salary'] == null
-                          ? "-"
-                          : numericFormatter.format(widget.maid['salary'])) +
-                      " " +
-                      AppLocalizations.of(context).tr('vnd_hour'),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  (widget.maid['distance'] != null
-                      ? AppLocalizations.of(context).tr('distance_display',
-                          args: [_getDistance(widget.maid['distance'])])
-                      : AppLocalizations.of(context).tr('no_location')),
-                ),
-                SizedBox(height: 5),
-                RatingBar(
-                  initialRating: (widget.maid['ratting'] ?? 0).toDouble(),
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemPadding: EdgeInsets.only(right: 4.0),
-                  itemSize: 20,
-                  itemBuilder: (context, _) => Icon(
-                    Icons.star,
-                    color: Colors.amber,
+            Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    widget.maid['name'] ?? "-",
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  ignoreGestures: true,
-                  onRatingUpdate: (double value) {},
-                ),
-              ],
+                  SizedBox(height: 5),
+                  Text(
+                    (widget.maid['salary'] == null
+                            ? "-"
+                            : numericFormatter.format(widget.maid['salary'])) +
+                        " " +
+                        AppLocalizations.of(context).tr('vnd_hour'),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    (widget.maid['distance'] != null
+                        ? AppLocalizations.of(context).tr('distance_display',
+                            args: [_getDistance(widget.maid['distance'])])
+                        : AppLocalizations.of(context).tr('no_location')),
+                  ),
+                  SizedBox(height: 5),
+                  RatingBar(
+                    initialRating: (widget.maid['ratting'] ?? 0).toDouble(),
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemPadding: EdgeInsets.only(right: 4.0),
+                    itemSize: 20,
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    ignoreGestures: true,
+                    onRatingUpdate: (double value) {},
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
